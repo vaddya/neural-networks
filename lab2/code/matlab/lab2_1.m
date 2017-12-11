2a6e3301288f33b9d332de2e3f3b2f1b755e6333
Vadim Dyachkov <vadik.dyachkov@gmail.com>
2017-12-11 17:55:07 +0300

    finish lab2

--- lab2/code/matlab/lab2_1.m
+++ lab2/code/matlab/lab2_1.m
@@ -1,4 +1,8 @@
+close all;
 load('5.mat');
+
+plot_func(P, T);
+saveas(gcf, '../../pics/1_1.png');
 
 linear_newlin(P, T);
 linear_newlind(P, T);
@@ -20,39 +24,49 @@ function linear_newlin(P, T)
     end
 
     plot_progress(P, T, X, Y);
-    saveas(gcf, '../../pics/1_2_1.png');
+    %saveas(gcf, '../../pics/1_2_1.png');
 end
 
 function linear_newlind(P, T)
     [Ptrain, Ttrain, ~, ~] = split(P, T);
     net = newlind(Ptrain, Ttrain);
     plot_model(net, P, T);
-    saveas(gcf, '../../pics/1_2_2.png');
+    %saveas(gcf, '../../pics/1_2_2.png');
 end
 
-function plot_model(net, P, T)
+function [Ptrain, Ttrain, Ptest, Ttest] = split(P, T)
+    [Train, Test, ~] = dividerand([P; T], 0.7, 0.3, 0);
+
+    Ptrain = Train(1, :);
+    Ttrain = Train(2, :);
+    Ptest = Test(1, :);
+    Ttest = Test(2, :);
+end
+
+function plot_func(P, T)
     figure;
     hold on;
-    plot(P, T, 'r');
-    X = 0 : 0.01 : 1;
-    Y = sim(net, X);
-    plot(X, Y);
+    plot(P, T);
     xlabel('X');
     ylabel('Y');
     axis([0 1 0 1]);
 end
 
-function plot_progress(P, T, X, Y)
-    figure;
+function plot_model(net, P, T)
     hold on;
-    plot(P, T);
+    plot_func(P, T);
+    X = 0 : 0.01 : 1;
+    Y = sim(net, X);
+    plot(X, Y);
+end
+
+function plot_progress(P, T, X, Y)
+    hold on;
+    plot_func(P, T);
     i = 1;
     while (i < 6)
         plot(X, Y(i, :));
         i = i + 1;
     end
-    xlabel('X');
-    ylabel('Y');
     legend('(P,T)', 'i=1', 'i=2', 'i=3', 'i=4');
-    axis([0 1 0 1]);
 end
